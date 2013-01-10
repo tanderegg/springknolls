31c9f3ab1ef58bd5a3f847a9dd0a50e52b99111b
Tim Anderegg <tim@neworganizing.com>
2013-01-10 15:21:05 -0500

    Added S3 to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,5 @@ Springknolls::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  Refinery::Core.config.s3_backend = true
 end
