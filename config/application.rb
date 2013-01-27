43e931ec623561027dd09bddfd14c2ae03b0ffec
Tim Anderegg <tim@neworganizing.com>
2013-01-27 00:11:40 -0500

    Attempt to fix s3 problem 2

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,9 @@ module Springknolls
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
+    ENV['S3_BUCKET'] = "springknolls_production"
+    ENV['S3_KEY'] = "AKIAIEJ4FKYB4HCBAY7Q"
+    ENV['S3_SECRET'] = "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"
     #config.assets.initialize_on_precompile = true
   end
 end
