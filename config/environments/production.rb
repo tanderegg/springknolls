0a8dabb0b0a033890f5d0efe4112ebe051faa539
Tim Anderegg <tim@neworganizing.com>
2013-01-26 23:55:33 -0500

    Attempt to fix s3 problem didn't work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Springknolls::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   Refinery::Core.config.s3_backend = true
-  Refinery.s3_backend = true
+  #Refinery.s3_backend = true
 end
