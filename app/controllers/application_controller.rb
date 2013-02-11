21008667e55533f2751ece9556f540f523ef3993
Tim Anderegg <tim@neworganizing.com>
2013-02-11 11:44:22 -0500

    Fixed page navigation for multi site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :get_root_page
+
+  def get_root_page
+    if params['path']
+      @params = params['path'].split('/')[0]
+      @root_page = Refinery::Page.find_by_slug(params['path'].split('/')[0])
+    else
+      @root_page = Refinery::Page.find_by_id(1)
+    end
+  end
 end
